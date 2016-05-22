571c122141abfb8a73b7f348584362fd41efcd3d
pogo <thoughts.stack@gmail.com>
2016-05-23 00:43:17 +0300

    url routing

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.qa.view import test
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', test),
+    url(r'^login/', test),
+    url(r'^signup/', test),
+    url(r'^question/\d*/', test),
+    url(r'^ask/', test),
+    url(r'^popular/', test),
+    url(r'^new/', test),
+    url(r'^admin/', test),
 ]
