66f50a44332d2ac75ccc34d96137f829e28ae43f
pogo <thoughts.stack@gmail.com>
2016-05-22 23:44:21 +0300

    more fix init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-cd /home/box/web
+bash cd /home/box/web
 sudo gunicorn -b 0.0.0.0:8080 /home/box/web/hello:app
 sudo /etc/init.d/nginx restart
