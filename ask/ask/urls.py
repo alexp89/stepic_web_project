2facff04e5cb23347b16c97f9d5222f0cdcd9a92
pogo <thoughts.stack@gmail.com>
2016-05-24 23:54:39 +0300

    fix error

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from qa.view import test
+from qa.views import test
 
 urlpatterns = [
     url(r'^$', test),
     url(r'^login/', test),
     url(r'^signup/', test),
-    url(r'^question/\d+/', test),
+    url(r'^question/(\d+)/$', test),
     url(r'^ask/', test),
     url(r'^popular/', test),
     url(r'^new/', test),
