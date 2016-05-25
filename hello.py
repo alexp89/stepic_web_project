263cce8cc4f2d1ca125e023aa932b52ca039cf5f
pogo <thoughts.stack@gmail.com>
2016-05-26 00:43:19 +0300

    fix shell scripts

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
-from cgi import parse_qs, escape
 def app(environ, start_response):
-  status = '200 OK'
-  headers = [
-    ('Content-Type', 'text/plain')
-  ]
-  start_response(status, headers)
-  resp = environ['QUERY_STRING'].split("&")
-  resp = [item+"\r\n" for item in resp]
-  return resp
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
